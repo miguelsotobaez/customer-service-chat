778251d4be1c39695d0e5c09824c4444621ed33b
Miguel Soto Baez <miguelsotobaez@gmail.com>
2024-08-17 21:34:35 +0100

    e2e test updated

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,13 @@
-import { Test, TestingModule } from '@nestjs/testing';
+// test/app.e2e-spec.ts
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +16,34 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/customer/available (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/customer/available')
       .expect(200)
-      .expect('Hello World!');
+      .expect((res) => {
+        expect(res.body).toHaveProperty('name');
+        expect(res.body).toHaveProperty('isAvailable');
+        expect(res.body.isAvailable).toBe(true);
+      });
+  });
+
+  it('/topics (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/topics')
+      .expect(200)
+      .expect((res) => {
+        expect(Array.isArray(res.body)).toBe(true);
+        expect(res.body.length).toBeGreaterThan(0);
+
+        // Verifica que hay un tema 'Football'
+        const footballTopic = res.body.find((topic) => topic.name === 'Football');
+        expect(footballTopic).toBeDefined();
+        expect(footballTopic).toHaveProperty('suggestions');
+        expect(footballTopic.suggestions.length).toBeGreaterThan(0);
+      });
   });
 });
