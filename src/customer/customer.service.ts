c32d342933ac0254201530e5aa456519750ad099
Miguel Soto Baez <miguelsotobaez@gmail.com>
2024-08-18 17:09:43 +0100

    added vercel config

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -7,9 +7,18 @@ export class CustomerService {
     { id: 1, name: 'Alice', isAvailable: true },
     { id: 2, name: 'Bob', isAvailable: false },
     { id: 3, name: 'Charlie', isAvailable: true },
+    { id: 4, name: 'Alan', isAvailable: true },
+    { id: 5, name: 'Sophia', isAvailable: true },
   ];
 
   getAvailableRepresentative(): CustomerServiceRepresentativeDto | null {
-    return this.representatives.find(rep => rep.isAvailable) || null;
+    const availableRepresentatives = this.representatives.filter(rep => rep.isAvailable);
+
+    if (availableRepresentatives.length === 0) {
+      return null; // No hay representantes disponibles
+    }
+
+    const randomIndex = Math.floor(Math.random() * availableRepresentatives.length);
+    return availableRepresentatives[randomIndex];
   }
 }
