274efef80a2eb3d1a117ab880cae56947f8544ad
Miguel Soto Baez <miguelsotobaez@gmail.com>
2024-08-18 14:25:31 +0100

    enable cors for localhost

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Habilitar CORS
+  app.enableCors({
+    origin: 'http://localhost:4200', // Asegúrate de que sea la URL correcta de tu frontend
+    methods: 'GET,POST,PUT,DELETE', // Métodos permitidos
+    allowedHeaders: 'Content-Type, Authorization', // Cabeceras permitidas
+  });
+  
   await app.listen(3000);
 }
 bootstrap();
